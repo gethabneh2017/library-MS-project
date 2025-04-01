3d3450d14517ce21c793e25a44984032a60653bb
Baba <bashameshebie21@gmail.com>
2025-04-01 11:25:56 +0300

    Update Library ms.cpp
    This is member definition from basha

--- Library ms.cpp
+++ Library ms.cpp
@@ -17,3 +17,19 @@ struct Book {
              << ", Genre: " << genre << ", Issued: " << (isIssued ? "Yes" : "No") << endl;
     }
 };
+//this is bashas
+class Member {
+public:
+    int id;
+    string name;
+    int borrowedBooks = 0;
+    bool isActive = true;
+    Member* next = nullptr;
+
+    Member(int id, const string& name) : id(id), name(name) {}
+
+    void display() const {
+        cout << "ID: " << id << ", Name: " << name
+             << ", Borrowed: " << borrowedBooks << ", Status: " << (isActive ? "Active" : "Inactive") << endl;
+    }
+};
