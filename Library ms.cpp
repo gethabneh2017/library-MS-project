b515ecf5f4f748491c3aa40bfe6a2467468207bb
tesfish29te <tesfayeteshale918@gmail.com>
2025-04-01 13:42:03 +0300

    Update Library ms.cpp
    It file handling and book functions implementation

--- Library ms.cpp
+++ Library ms.cpp
@@ -44,6 +44,147 @@ public:
 };
 
 
+//From Tesfaye Teshale
+
+// Library system
+class Library {
+private:
+    static const int MAX_BOOKS = 1000;
+    Book books[MAX_BOOKS];
+    int bookCount = 0;
+    Member* membersHead = NULL;
+    const string BOOKS_FILE = "books.dat";
+    const string MEMBERS_FILE = "members.dat";
+
+public:
+    Library() {
+        loadBooks();
+        loadMembers();
+    }
+
+    ~Library() {
+        saveBooks();
+        saveMembers();
+        clearMembers();
+    }
+
+    void loadBooks() {
+        ifstream file(BOOKS_FILE, ios::binary);
+        if (!file) return;
+
+        file.read((char*)&bookCount, sizeof(bookCount));
+        for (int i = 0; i < bookCount; i++) {
+            file.read((char*)&books[i], sizeof(Book));
+        }
+        file.close();
+    }
+
+    void saveBooks() {
+        ofstream file(BOOKS_FILE, ios::binary);
+        if (!file) {
+            cerr << "Error saving books data!\n";
+            return;
+        }
+
+        file.write((char*)&bookCount, sizeof(bookCount));
+        for (int i = 0; i < bookCount; i++) {
+            file.write((char*)&books[i], sizeof(Book));
+        }
+        file.close();
+    }
+
+    void loadMembers() {
+        ifstream file(MEMBERS_FILE, ios::binary);
+        if (!file) return;
+
+        int memberCount;
+        file.read((char*)&memberCount, sizeof(memberCount));
+
+        for (int i = 0; i < memberCount; i++) {
+            int id, borrowedBooks;
+            bool isActive;
+            string name;
+
+            file.read((char*)&id, sizeof(id));
+            file.read((char*)&borrowedBooks, sizeof(borrowedBooks));
+            file.read((char*)&isActive, sizeof(isActive));
+
+            size_t nameSize;
+            file.read((char*)&nameSize, sizeof(nameSize));
+            name.resize(nameSize);
+            file.read(&name[0], nameSize);
+
+            Member* newMember = new Member(id, name);
+            newMember->borrowedBooks = borrowedBooks;
+            newMember->isActive = isActive;
+            newMember->next = membersHead;
+            membersHead = newMember;
+        }
+        file.close();
+    }
+
+    void saveMembers() {
+        ofstream file(MEMBERS_FILE, ios::binary);
+        if (!file) {
+            cerr << "Error saving members data!\n";
+            return;
+        }
+
+        int memberCount = 0;
+        Member* temp = membersHead;
+        while (temp) {
+            memberCount++;
+            temp = temp->next;
+        }
+
+        file.write((char*)&memberCount, sizeof(memberCount));
+
+        temp = membersHead;
+        while (temp) {
+            file.write((char*)&temp->id, sizeof(temp->id));
+            file.write((char*)&temp->borrowedBooks, sizeof(temp->borrowedBooks));
+            file.write((char*)&temp->isActive, sizeof(temp->isActive));
+
+            size_t nameSize = temp->name.size();
+            file.write((char*)&nameSize, sizeof(nameSize));
+            file.write(temp->name.c_str(), nameSize);
+
+            temp = temp->next;
+        }
+        file.close();
+    }
+void clearMembers() {
+        while (membersHead) {
+            Member* temp = membersHead;
+            membersHead = membersHead->next;
+            delete temp;
+        }
+    }
+
+
+void addBook(int id, string title, string author, string genre) {
+        if (bookCount < MAX_BOOKS) {
+            books[bookCount++] = {id, title, author, genre, false};
+            cout << "Book added successfully!\n";
+            saveBooks();
+        } else {
+            cout << "Library is full!\n";
+        }
+    }
+
+    void searchBook(string title) {
+        bool found = false;
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].title == title) {
+                books[i].display();
+                found = true;
+            }
+        }
+        if (!found) {
+            cout << "Book not found!\n";
+        }
+}
+
 //this is from banderaw
 int main() {
     Library lib;
